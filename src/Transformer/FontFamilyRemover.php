aec32c66befad976dbd1cb2f1660100b52ec47cb
Michaël Perrin <contact@michaelperrin.fr>
2020-08-27 17:26:38 -0500

    Remove font size

--- src/Transformer/FontFamilyRemover.php
+++ src/Transformer/FontFamilyRemover.php
@@ -7,6 +7,7 @@ class FontFamilyRemover implements TransformerInterface
     public function transform(string $content): string
     {
         $content = $this->removeFontFamily($content);
+        $content = $this->removeFontSizes($content);
         $content = $this->removeSomeColors($content);
         $content = $this->removeEmptyStyle($content);
 
@@ -16,6 +17,11 @@ class FontFamilyRemover implements TransformerInterface
     protected function removeFontFamily(string $content): string
     {
         return preg_replace('|font-family:[^";]*|', '', $content);
+    }
+
+    protected function removeFontSizes(string $content): string
+    {
+        return preg_replace('|font-size:[^";]*|', '', $content);
     }
 
     protected function removeSomeColors(string $content): string
